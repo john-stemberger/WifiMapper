a1ee19c735da305345f80dc1aab498cedfbe3184
John <john@john.com>
2017-06-22 06:44:38 -0400

    added permissions, scanning, and firebase integration

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 TODO: integarte with firebase
 TODO: integrate with google maps api
 TODO: define schema for wifi points
-       - sid, location, date of encounter, strength, public/private, encryption, etc
-TODO: 
+       - sid, location, date of encounter, strength, public/private, encryption, sign in response, speed test results
+TODO:
---
+++ app/src/main/java/john/com/wifimapper/MainActivity.java
@@ -0,0 +1,158 @@
+package john.com.wifimapper;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity
+        extends AppCompatActivity
+        implements View.OnClickListener
+{
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
+    WifiManager wifi;
+    ListView lv;
+    TextView textStatus;
+    Button buttonScan;
+    int size = 0;
+    List<ScanResult> results;
+    BroadcastReceiver scanReceiver;
+
+
+    String ITEM_KEY = "key";
+    ArrayList<HashMap<String, String>> arraylist = new ArrayList<HashMap<String, String>>();
+    SimpleAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        textStatus = (TextView) findViewById(R.id.textStatus);
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+        buttonScan.setOnClickListener(this);
+        lv = (ListView) findViewById(R.id.list);
+
+        wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (wifi.isWifiEnabled() == false)
+        {
+            Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled", Toast.LENGTH_LONG).show();
+            wifi.setWifiEnabled(true);
+        }
+//        this.adapter = new SimpleAdapter(this, arraylist, R.layout.row, new String[]{ITEM_KEY}, new int[]{R.id.list_value});
+//        lv.setAdapter(this.adapter);
+
+        scanReceiver = new BroadcastReceiver()
+        {
+            @Override
+            public void onReceive(Context c, Intent intent)
+            {
+                onResults(c, intent);
+            }
+        };
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        checkForPermissions();
+        registerReceiver(scanReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+    }
+
+    private void checkForPermissions()
+    {
+        List<String> permissionsList = new ArrayList<>();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(Manifest.permission.ACCESS_WIFI_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CHANGE_WIFI_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(Manifest.permission.CHANGE_WIFI_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+
+        if (permissionsList.size() > 0) {
+            ActivityCompat.requestPermissions(this, permissionsList.toArray(new String[permissionsList.size()]),
+                    PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        unregisterReceiver(scanReceiver);
+    }
+
+    private void onResults(Context context, Intent intent)
+    {
+        results = wifi.getScanResults();
+        size = results.size();
+        if (results == null)
+        {
+            return;
+        }
+        for (ScanResult r : results)
+        {
+            Log.d(TAG, "" + r);
+        }
+    }
+
+    /**
+     * View.OnClickListener
+     **/
+    public void onClick(View view)
+    {
+        arraylist.clear();
+        wifi.startScan();
+//        Toast.makeText(this, "Scanning...." + size, Toast.LENGTH_SHORT).show();
+//        try
+//        {
+//            size = size - 1;
+//            while (size >= 0)
+//            {
+//                HashMap<String, String> item = new HashMap<String, String>();
+//                item.put(ITEM_KEY, results.get(size).SSID + "  " + results.get(size).capabilities);
+//                arraylist.add(item);
+//                size--;
+////                adapter.notifyDataSetChanged();
+//            }
+//        }
+//        catch (Exception e)
+//        {
+//        }
+    }
+
+}
